44e75f9e16145088b24afb3fdf0a3cb8d646c9c6
Fitz-commits <christopher.delaby@hotmail.com>
2025-04-11 14:03:11 +0200

    Adding implementation on Type comparer Function declaration Assign with type

--- src/main/java/be/unamur/info/b314/compiler/emj/EMJSymbolInfo.java
+++ src/main/java/be/unamur/info/b314/compiler/emj/EMJSymbolInfo.java
@@ -1,11 +1,16 @@
 package be.unamur.info.b314.compiler.emj;
 
+import java.util.List;
+
 public class EMJSymbolInfo {
     private final String id;
     private final String dataType;
     private final String scope;
     private final EMJSymbolType symbolType;
     private boolean isInitialized;
+
+    private List<EMJParameterInfo> parameters;  // Paramètres de la fonction
+    private String returnType;              // Type de retour
 
     EMJSymbolInfo(String id, String dataType, String scope, EMJSymbolType symbolType, boolean initialized) {
         this.id = id;
@@ -14,4 +19,25 @@ public class EMJSymbolInfo {
         this.symbolType = symbolType;
         this.isInitialized = initialized;
     }
+
+    public String getId() {
+        return id;
+    }
+
+    public void SetReturnType(String returnType) {
+        this.returnType = returnType;
+    }
+
+    public void SetParameters(List<EMJParameterInfo> parameters) {
+        this.parameters = parameters;
+    }
+    public String getType() {
+        return dataType;
+    }
+
+    public String getReturnType() {
+        return returnType;
+    }
 }
+
+
